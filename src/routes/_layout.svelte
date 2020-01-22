478e70cff5b7606d1386067f9f060c3e9ad5d0d8
Khaled Pendleton <khaled@Khaleds-MacBook-Pro.local>
2020-01-22 14:56:25 -0500

    Removed markup that belonged in all layouts to _layout.svelte

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -2,4 +2,35 @@
 	export let segment;
 </script>
 
-<slot></slot>
+<header>
+	<div class="wrapper">
+		<div>
+			<a href=".">Khaled Pendleton</a>
+			<h1>A design-driven web developer from New York</h1>
+		</div>
+	</div>
+</header>
+
+<main>
+	<slot></slot>
+</main>
+
+<footer>
+	<div class="wrapper">
+		<div>
+			<section>
+				<h6>Social Media</h6>
+				<ul>
+					<li><a href="#">Tw</a></li>
+					<li><a href="#">Ig</a></li>
+					<li><a href="#">Gh</a></li>
+				</ul>
+			</section>
+
+			<section>
+				<h6>Email</h6>
+				<a href="mailto:hi@khaledpendleton.com">hi@khaledpendleton.com</a>
+			</section>
+		</div>
+	</div>
+</footer>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,31 +2,5 @@
 	<title>KHALED PENDLETON</title>
 </svelte:head>
 
-<header>
-	<div class="wrapper">
-		<div>
-			<a href=".">Khaled Pendleton</a>
-			<h1>A design-driven web developer from New York</h1>
-		</div>
-	</div>
-</header>
 
-<footer>
-	<div class="wrapper">
-		<div>
-			<section>
-				<h6>Social Media</h6>
-				<ul>
-					<li><a href="#">Tw</a></li>
-					<li><a href="#">Ig</a></li>
-					<li><a href="#">Gh</a></li>
-				</ul>
-			</section>
 
-			<section>
-				<h6>Email</h6>
-				<a href="mailto:hi@khaledpendleton.com">hi@khaledpendleton.com</a>
-			</section>
-		</div>
-	</div>
-</footer>
