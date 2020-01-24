2f83ee3c4329cc65e4e41b029bee15d33b25436a
Khaled Pendleton <khaled@Khaleds-MacBook-Pro.local>
2020-01-24 11:57:02 -0500

    Nav and SocialsList components for layout

---
+++ src/components/Nav.svelte
@@ -0,0 +1,17 @@
+<style>
+    nav {
+		display: flex;
+		justify-content: space-between;
+		padding-top: var(--spacing);
+		padding-bottom: var(--spacing);
+    }
+    
+    a {
+        color: inherit;
+    }
+</style>
+
+<nav>
+    <a href=".">Khaled Pendleton</a>
+    <a href="mailto:hi@khaledpendleton.com">Contact</a>
+</nav>
