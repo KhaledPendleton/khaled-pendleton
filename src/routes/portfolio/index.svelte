de850b18e592db3eae44cf3e2e1a9740138ade1a
Khaled Pendleton <khaled@Khaleds-MacBook-Pro.local>
2020-01-28 16:09:02 -0500

    Created route for portfolio

---
+++ src/routes/portfolio/index.svelte
@@ -0,0 +1,20 @@
+<svelte:head>
+    <title>KHALED PENDLETON | PORTFOLIO</title>
+</svelte:head>
+
+<script context="module">
+    export async function preload({ params, query }) {
+        const response = await this.fetch('portfolio.json');
+        const projects = await response.json();
+        return { projects };
+    }
+</script>
+
+<script>
+    import ContentList from '../../components/ContentList.svelte';
+    export let projects;
+</script>
+
+<section class="box">
+    <ContentList content={projects}>Projects</ContentList>
+</section>
