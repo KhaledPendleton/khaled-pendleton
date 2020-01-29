d40f7db1e02e3d03761facb084ee222fc3621344
Khaled Pendleton <khaled@Khaleds-MacBook-Pro.local>
2020-01-29 00:08:01 -0500

    Thumbnail component for blog post ATFs

---
+++ src/components/Thumbnail.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let src;
+    export let alt = 'No alt given';
+</script>
+
+<style>
+    figure {
+		margin: 0;
+	}
+
+	figcaption {
+		font-size: 0.75em;
+		text-transform: uppercase;
+    }
+    
+    img {
+        width: 100%;
+		max-width: 100%;
+		height: auto;
+    }
+</style>
+
+<figure class="box">
+    <img {src} {alt}>
+    <figcaption>{ alt }</figcaption>
+</figure>
