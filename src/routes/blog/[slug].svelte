51e05b9b0e600adf0e909bd0eb2cdd40e3ddc0b9
Khaled Pendleton <khaled@Khaleds-MacBook-Pro.local>
2020-01-29 00:07:20 -0500

    Added support for publish date and thumbnail

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -10,16 +10,21 @@
 		if (response.status === 200) {
 			return { post };
 		}
-			
+
 		this.error(response.status, post.message);
 	}
 </script>
 
 <script>
+	import Thumbnail from '../../components/Thumbnail.svelte';
 	export let post;
 </script>
 
 <style>
+	.date {
+		text-transform: uppercase;
+	}
+
 	@media screen and (min-width: 400px) {
 		article {
 			font-size: 1.25em;
@@ -35,7 +40,14 @@
 </style>
 
 <header class="atf box">
+	{#if post.metadata.thumbnail }
+		<Thumbnail
+			src="images/{ post.metadata.thumbnail }"
+			alt="{ post.metadata['thumbnail-alt'] }"
+		/>
+	{/if}
 	<h1 class="title">{ post.metadata.title }</h1>
+	<p class="date">{ post.metadata.dateString }</p>
 </header>
 
 <section class='box'>
