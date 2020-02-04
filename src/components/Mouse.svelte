ef091155f047843b1c8e24b473be047435437d9b
Khaled Pendleton <khaled@Khaleds-MacBook-Pro.local>
2020-02-03 19:35:20 -0500

    Hide mouse follower on non-mouse devices

--- src/components/Mouse.svelte
+++ src/components/Mouse.svelte
@@ -21,20 +21,24 @@
 </script>
 
 <style>
+    svg {
+        display: none;
+    }
+
     @media (pointer: fine) {
         svg {
             width: 100vw;
             height: 100vh;
+            top: 0; left: 0;
             position: fixed;
-            top: 0;
-            left: 0;
+            display: initial;
             pointer-events: none;
         }
 
         circle {
             fill: none;
-            stroke: var(--dark-orange);
             stroke-width: 2px;
+            stroke: var(--dark-orange);
         }
     }
 </style>
